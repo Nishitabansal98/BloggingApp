845ba90e712c40e3c463d426b68db5a596220289
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2023-09-27 14:28:15 +0530

    Start Project

---
+++ blog-app-apis/src/test/java/com/codewithnishita/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithnishita.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
